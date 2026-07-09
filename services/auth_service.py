d8949a942f05c57e8733c9fcf6d0ce9ea0185b09
ZayEric <122373288+ZayEric@users.noreply.github.com>
2026-07-09 15:36:08 +0630

    Create auth_service.py

---
+++ services/auth_service.py
@@ -0,0 +1,22 @@
+import requests
+from config import *
+
+def get_graph_token():
+
+    url = f"https://login.microsoftonline.com/{TENANT_ID}/oauth2/v2.0/token"
+
+    data = {
+
+        "client_id": CLIENT_ID,
+
+        "client_secret": CLIENT_SECRET,
+
+        "scope": "https://graph.microsoft.com/.default",
+
+        "grant_type": "client_credentials"
+
+    }
+
+    response = requests.post(url,data=data)
+
+    return response.json()["access_token"]
