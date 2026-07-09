4d1340d1520b3890ea647dc413a9592d3360fd7b
ZayEric <122373288+ZayEric@users.noreply.github.com>
2026-07-09 15:29:16 +0630

    Create config.py

---
+++ config.py
@@ -0,0 +1,15 @@
+import os
+
+TENANT_ID = os.getenv("TENANT_ID")
+
+CLIENT_ID = os.getenv("CLIENT_ID")
+
+CLIENT_SECRET = os.getenv("CLIENT_SECRET")
+
+SITE_ID = os.getenv("SITE_ID")
+
+DRIVE_ID = os.getenv("DRIVE_ID")
+
+FILE_PATH = os.getenv("FILE_PATH")
+
+OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
