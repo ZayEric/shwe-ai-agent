2670065a223202debca20e28fb507621f7001660
ZayEric <122373288+ZayEric@users.noreply.github.com>
2026-07-15 15:10:23 +0630

    Refactor Azure OpenAI client and add explain_result function
    Refactored Azure OpenAI client initialization and added explain_result function for processing search results.

--- services/openai_service.py
+++ services/openai_service.py
@@ -1,11 +1,46 @@
 from openai import AzureOpenAI
+
 from config import *
 
-raise Exception(
-    f"""
-Endpoint={repr(AZURE_OPENAI_ENDPOINT)}
-Deployment={repr(AZURE_OPENAI_DEPLOYMENT)}
-Version={repr(AZURE_OPENAI_API_VERSION)}
-Key={AZURE_OPENAI_KEY is not None}
-"""
+client = AzureOpenAI(
+    api_key=AZURE_OPENAI_KEY,
+    api_version=AZURE_OPENAI_API_VERSION,
+    azure_endpoint=AZURE_OPENAI_ENDPOINT
 )
+
+
+def explain_result(search_result):
+    print("========== Azure Config ==========")
+    print("Endpoint:", repr(AZURE_OPENAI_ENDPOINT))
+    print("Deployment:", repr(AZURE_OPENAI_DEPLOYMENT))
+    print("API Version:", repr(AZURE_OPENAI_API_VERSION))
+    print("Key exists:", AZURE_OPENAI_KEY is not None)
+    print("==================================")
+    prompt = f"""
+    You are an AML Compliance Officer.
+    
+    Explain this blacklist screening result.
+    
+    {search_result}
+    
+    Provide:
+    1. Match summary
+    2. Risk level
+    3. Recommendation
+    """
+    print(AZURE_OPENAI_ENDPOINT)
+    print(AZURE_OPENAI_DEPLOYMENT)
+    print(client.base_url)
+    print("Calling Azure OpenAI...")
+    response = client.chat.completions.create(
+        model=AZURE_OPENAI_DEPLOYMENT,
+        messages=[
+            {
+                "role": "user",
+                "content": prompt
+            }
+        ],
+        temperature=0.2
+    )
+
+    return response.choices[0].message.content
