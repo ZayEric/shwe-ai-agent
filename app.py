df7138873c889f81335367a4b435a329504f91ab
ZayEric <122373288+ZayEric@users.noreply.github.com>
2026-07-17 13:28:33 +0630

    Include screening router in FastAPI app

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
 from fastapi import FastAPI
-
 from routers.blacklist import router
+from routers import screening
 
 app=FastAPI()
 
 app.include_router(router)
+app.include_router(screening.router)
