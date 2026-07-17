71cdbed113e763a9fee528662165f75f32bb9ade
ZayEric <122373288+ZayEric@users.noreply.github.com>
2026-07-17 13:23:52 +0630

    Enhance get_blacklist with data cleaning and formatting
    Refactor get_blacklist function to clean and format data.

--- repositories/blacklist_repository.py
+++ repositories/blacklist_repository.py
@@ -1,3 +1,28 @@
-def load():
+import numpy as np
 
-    return load_blacklist()
+from services.sharepoint_service import load_blacklist
+
+
+def get_blacklist():
+
+    df = load_blacklist()
+
+    df = df.replace([np.nan, np.inf, -np.inf], None)
+
+    df.columns = df.columns.str.strip()
+
+    df["Name"] = (
+
+        df["Name"]
+
+        .fillna("")
+
+        .astype(str)
+
+        .str.strip()
+
+        .str.upper()
+
+    )
+
+    return df
