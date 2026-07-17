f17fbab75966cdcc51b53606d7bdfe73b20094bf
ZayEric <122373288+ZayEric@users.noreply.github.com>
2026-07-17 13:24:44 +0630

    Refactor get_ibmb to clean DataFrame
    Refactor get_ibmb function to clean DataFrame and handle NaN values.

--- repositories/ibmb_repository.py
+++ repositories/ibmb_repository.py
@@ -1,3 +1,28 @@
-def load():
+import numpy as np
 
-    return load_ibmb()
+from services.sharepoint_service import load_ibmb
+
+
+def get_ibmb():
+
+    df = load_ibmb()
+
+    df = df.replace([np.nan, np.inf, -np.inf], None)
+
+    df.columns = df.columns.str.strip()
+
+    df["Name"] = (
+
+        df["Name"]
+
+        .fillna("")
+
+        .astype(str)
+
+        .str.strip()
+
+        .str.upper()
+
+    )
+
+    return df
