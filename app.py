9e01a05987e4e0edd6bc6ce0885cbb0aaa4e96c8
ZayEric <122373288+ZayEric@users.noreply.github.com>
2026-07-20 12:13:14 +0630

    Fix formatting and import customer_insight router

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
 from fastapi import FastAPI
 from routers.blacklist import router
 from routers import screening
+from routers import customer_insight
 
-app=FastAPI()
+app = FastAPI()
 
 app.include_router(router)
 app.include_router(screening.router)
+app.include_router(customer_insight.router)
