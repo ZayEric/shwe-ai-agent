7d3bc6ca2a0bad1cdca07e1a71e88d5166d66cf7
ZayEric <122373288+ZayEric@users.noreply.github.com>
2026-07-20 12:25:27 +0630

    Add DocumentLoader class for loading SharePoint documents
    Implement DocumentLoader class to load various document types from SharePoint.

---
+++ services/document_loader.py
@@ -0,0 +1,39 @@
+from config import *
+
+from services.customer_sharepoint_service import (
+    CustomerSharePointService
+)
+
+
+class DocumentLoader:
+
+    def __init__(self):
+
+        self.sp = CustomerSharePointService()
+
+    def load_all(self):
+
+        return {
+
+            "facebook":
+                self.sp.load_json(FACEBOOK_PATTERN),
+
+            "playstore":
+                self.sp.load_json(PLAYSTORE_PATTERN),
+
+            "competitor":
+                self.sp.load_markdown(COMPETITOR_PATTERN),
+
+            "wallet":
+                self.sp.load_excel(WALLET_PATTERN),
+
+            "ibmb":
+                self.sp.load_excel(IBMB_PATTERN),
+
+            "campaign":
+                self.sp.load_excel(CAMPAIGN_PATTERN),
+
+            "customer":
+                self.sp.load_excel(CUSTOMER_PATTERN)
+
+        }
